31e764b10c807b21d6660e897e1dde8a986f4644
fengge <1059387928@qq.com>
2017-12-19 17:40:51 +0800

    1

--- teacher/ali/readExcel.py
+++ teacher/ali/readExcel.py
@@ -1,53 +1,85 @@
 
 import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error as mse
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import  train_test_split as test
+#--------------------将特征变为数字--------------------
+# data=pd.read_excel("data/训练.xlsx",sheetname=0)
+# label=data.columns[-1]
+# feature=list(data.columns[1:-1])
+# X_all = data[feature]
+# y_all = data[label]
+#
+# def preprocess_features(X):
+#     output = pd.DataFrame(index=X.index)
+#     for col, col_data in X.iteritems():
+#         if col_data.dtype == object:
+#             col_data = col_data.replace(['yes', 'no'], [1, 0])
+#         if col_data.dtype == object:
+#             col_data = pd.get_dummies(col_data, prefix=col)
+#         output = output.join(col_data)
+#     return output
+#
+# print(X_all.head())
+# X_all = preprocess_features(X_all)
+# result = pd.concat([X_all, y_all], axis=1)
+# result.to_csv("data/value_1.csv")
 
-data=pd.read_excel("data/训练.xlsx",sheetname=0)
+
+#--------------------去除无用特征--------------------
+# data=pd.read_csv("data/value_1.csv")
+# label=data.columns[-1]
+# feature=list(data.columns[1:-1])
+# X_all = data[feature]
+# y_all = data[label]
+# print(X_all.head())
+# delete_col=[]
+# for i in range(len(X_all.columns)):
+#     col_name = X_all.columns[i]
+#     col = X_all[col_name]
+#     dic = {}
+#     for v in col:
+#         if v == v:
+#             dic[str(v)] = 1
+#     if len(dic) <= 1:
+#         delete_col.append(col_name)
+#         print(X_all.columns[i])
+# df = pd.DataFrame(delete_col, columns=['col'])
+# df.to_csv("data/delete_cols.csv")
+# X_all = X_all.drop(columns=delete_col)
+# print(X_all.head())
+# result = pd.concat([X_all, y_all], axis=1)
+# result.to_csv("data/delete_unuse_feature_2.csv")
+
+#---------------------处理缺省值----------------------
+# data=pd.read_csv("data/delete_unuse_feature_2.csv")
+# # data=data.fillna(0)
+# data=data.fillna(data.mean())
+# data.to_csv("data/nan_mean_3.csv")
+
+# df = pd.read_csv('pooja.csv')
+# df_norm = (df - df.mean()) / (df.max() - df.min())
+# df_norm.to_csv('example.csv')
+
+#---------------------训练预测----------------------
+
+data=pd.read_csv("data/nan_mean_3.csv")
+pre_a=pd.read_excel("data/测试A.xlsx")
+
 label=data.columns[-1]
 feature=list(data.columns[1:-1])
-
 X_all = data[feature]
 y_all = data[label]
-col_name = X_all.columns[-1]
-col=X_all[col_name]
-dic = {}
-for v in col:
-    if v==v:
-        dic[str(v)] = 1
-        print(v)
-if len(dic) <= 1:
-    print(X_all.columns[-1])
-def preprocess_features(X):
-    ''' Preprocesses the student data and converts non-numeric binary variables into
-        binary (0/1) variables. Converts categorical variables into dummy variables. '''
+x=np.array(X_all)
+y=np.array(y_all)
 
-    # Initialize new output DataFrame
-    output = pd.DataFrame(index=X.index)
+model = LinearRegression()
+model.fit(x,y)
 
-    # Investigate each feature column for the data
-    for col, col_data in X.iteritems():
-
-        # If data type is non-numeric, replace all yes/no values with 1/0
-        if col_data.dtype == object:
-            col_data = col_data.replace(['yes', 'no'], [1, 0])
-
-        # If data type is categorical, convert to dummy variables
-        if col_data.dtype == object:
-            # Example: 'school' => 'school_GP' and 'school_MS'
-            col_data = pd.get_dummies(col_data, prefix=col)
-
-            # Collect the revised columns
-        output = output.join(col_data)
-
-    return output
+predictions = model.predict(X_test)
+loss=mse(predictions,y_test)
+print(loss/len(y_test))
 
 
-X_all = preprocess_features(X_all)
-for i in range(len(X_all.columns)):
-    col=X_all.columns[i]
-    dic={}
-    for v in col:
-        dic[str(v)]=1
-    if len(dic)<=1:
-        print(X_all.columns[i])
-print(X_all.head())
-print("Processed feature columns ({} total features):\n{}".format(len(X_all.columns), list(X_all.columns)))
