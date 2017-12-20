80884f84e032c3b15ead78ff04f87e9339381199
fengge <1059387928@qq.com>
2017-12-20 17:32:50 +0800

    1

--- teacher/ali/test.py
+++ teacher/ali/test.py
@@ -1,34 +1,4 @@
 
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
 
-def runplt():
-    plt.figure()
-    plt.title(u'diameter-cost curver')
-    plt.xlabel(u'diameter')
-    plt.ylabel(u'cost')
-    plt.axis([0, 25, 0, 25])
-    plt.grid(True)
-    return plt
 
-X = [[6, 2], [8, 1], [10, 0], [14, 2], [18, 0]]
-y = [[7], [9], [13], [17.5], [18]]
-model = LinearRegression()
-model.fit(X, y)
-X_test = [[8, 2], [9, 0], [11, 2], [16, 2], [12, 0]]
-y_test = [[11], [8.5], [15], [18], [11]]
-predictions = model.predict(X_test)
-for i, prediction in enumerate(predictions):
-    print('Predicted: %s, Target: %s' % (prediction, y_test[i]))
-print('R-squared: %.2f' % model.score(X_test, y_test))
-quadratic_featurizer = PolynomialFeatures(degree=2)
-X_train_quadratic = quadratic_featurizer.fit_transform(X)
-X_test_quadratic = quadratic_featurizer.transform(X)
-regressor_quadratic = LinearRegression()
-regressor_quadratic.fit(X_train_quadratic, y)
-predictions=regressor_quadratic.predict(X_test_quadratic)
-for i, prediction in enumerate(predictions):
-    print('Predicted: %s, Target: %s' % (prediction, y_test[i]))
-print('R-squared: %.2f' % regressor_quadratic.score(X_test_quadratic, y_test))
+
