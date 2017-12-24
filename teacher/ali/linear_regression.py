a3fbfff35125e4181d4dbb3cb9fcd7c37ad5c870
fengges <1059387928@qq.com>
2017-12-24 22:44:51 +0800

    Merge branch 'master' of github.com:fengges/teacher
    # Conflicts:
    #	teacher/ali/linear_regression.py

--- teacher/ali/linear_regression.py
+++ teacher/ali/linear_regression.py
@@ -138,14 +138,16 @@ def getDegree(m):
 
 model = LinearRegression(normalize=True)
 # model.fit(X_train,y_train)
-quadratic_featurizer = PolynomialFeatures(degree=2)
+quadratic_featurizer = PolynomialFeatures(degree=3)
 X_train_quadratic = quadratic_featurizer.fit_transform(t)
 X_a_quadratic = quadratic_featurizer.transform(t_a)
 X_b_quadratic = quadratic_featurizer.transform(t_b)
 model.fit(X_train_quadratic,y_all)
+pre=model.predict(X_train_quadratic)
 pre_a = model.predict(X_a_quadratic)
 pre_b = model.predict(X_b_quadratic)
-
+loss=mse(pre,y_all)
+print(loss)
 #--------------------生成答案---------------------------
 
 ans_a_label=pd.read_csv("data/ans_a.csv")
